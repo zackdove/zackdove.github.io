fa5f8f4fd59f152e462a4b4045c0be1009276967
zack dove <zdove@live.co.uk>
2020-12-22 10:40:00 +0000

    Finished main page

--- lightsToggle.js
+++ lightsToggle.js
@@ -1,30 +1,33 @@
 var lightsOn = 0;
 function toggleLights(){
-	var lightsToggleLabel = document.getElementById("lightsToggleLabel");
-	var darkContainer = document.getElementById("darkContainer");
-	var spotlight = document.getElementById("spotlight");
-	var spotlight_child = document.getElementById("spotlight-child");
-	if (lightsOn == 1){
-		// turn off
-		console.log("turning lights off");
-		lightsOn = 0;
-		lightsToggleLabel.innerHTML = "click to turn the lights on";
-		darkContainer.style.setProperty("background", "#222222");
-		spotlight.style.setProperty("box-shadow", "0 0 0 10000px rgba(0, 0, 0, 1.0)")
-		darkContainer.style.setProperty("color", "#00aa00");
-		spotlight_child.style.setProperty("box-shadow", "inset 0px 0px 200px 75px rgba(0,0,0,1.0)");
-		let gradientContainer = document.getElementById("gradientContainer2");
-		gradientContainer.style.setProperty("background", "linear-gradient(180deg, rgba(30,106,195,1) 0%, black 100%)")
-	} else {
-		// turn on
-		lightsOn = 1;
-		console.log("turnjng lights on");
-		lightsToggleLabel.innerHTML = "click to turn the lights off"
-		darkContainer.style.setProperty("background", "rgb(41, 127, 150)");
-		let gradientContainer = document.getElementById("gradientContainer2");
-		gradientContainer.style.setProperty("background", "linear-gradient(180deg, rgba(30,106,195,1) 0%, rgb(41, 127, 150) 100%)")
-		darkContainer.style.setProperty("color", "#EEEEEE");
-		spotlight.style.setProperty("box-shadow", "none")
-		spotlight_child.style.setProperty("box-shadow", "none")
+	let mouseQuery = window.matchMedia("(hover: hover) and (pointer: fine)");
+	if (mouseQuery.matches){
+		var lightsToggleLabel = document.getElementById("lightsToggleLabel");
+		var darkContainer = document.getElementById("darkContainer");
+		var spotlight = document.getElementById("spotlight");
+		var spotlight_child = document.getElementById("spotlight-child");
+		if (lightsOn == 1){
+			// turn off
+			console.log("turning lights off");
+			lightsOn = 0;
+			lightsToggleLabel.innerHTML = "click to turn the lights on";
+			darkContainer.style.setProperty("background", "#222222");
+			spotlight.style.setProperty("box-shadow", "0 0 0 10000px rgba(0, 0, 0, 1.0)")
+			darkContainer.style.setProperty("color", "#00aa00");
+			spotlight_child.style.setProperty("box-shadow", "inset 0px 0px 200px 75px rgba(0,0,0,1.0)");
+			let gradientContainer = document.getElementById("gradientContainer2");
+			gradientContainer.style.setProperty("background", "linear-gradient(180deg, rgba(30,106,195,1) 0%, black 100%)")
+		} else {
+			// turn on
+			lightsOn = 1;
+			console.log("turnjng lights on");
+			lightsToggleLabel.innerHTML = "click to turn the lights off"
+			darkContainer.style.setProperty("background", "rgb(41, 127, 150)");
+			let gradientContainer = document.getElementById("gradientContainer2");
+			gradientContainer.style.setProperty("background", "linear-gradient(180deg, rgba(30,106,195,1) 0%, rgb(41, 127, 150) 100%)")
+			darkContainer.style.setProperty("color", "#EEEEEE");
+			spotlight.style.setProperty("box-shadow", "none")
+			spotlight_child.style.setProperty("box-shadow", "none")
+		}
 	}
 }
